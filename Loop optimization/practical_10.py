64cbe50f8581b216fda7791810452b97b936da1a
anurag9601 <150320727+anurag9601@users.noreply.github.com>
2024-09-24 17:14:21 +0530

    Update practical_10.py

--- Loop optimization/practical_10.py
+++ Loop optimization/practical_10.py
@@ -1,17 +1,22 @@
 #Loop optimization 
 
 import time
-
-t1  = time.time()
-for i in range(1,100+1):
-    print(i,end=" ")
+t1 = time.time()
+time.sleep(.1)
+for i in range(1, 100+1):
+    print(i, end=" ")
 t2 = time.time()
-print(f"Total execution taken by first function is {t2-t1}")
-for i in range(1,100+1):
-    print(i,end=" ")
-    print(i+1,end=" ")
-    print(i+2,end=" ")
-    print(i+3,end=" ")
-    print(i+4,end=" ")
+print()
+print(f"Process time: {t2 - t1:6f}")
+time.sleep(.1)
+for i in range(1, 100+1):
+    print(i, end=" ")
+    print(i+1, end=" ")
+    print(i+2, end=" ")
+    print(i+3, end=" ")
+    print(i+4, end=" ")
 t3 = time.time()
-print(f"Total execution taken by second function is {t3-t2}")
+print()
+print(f"Process time: {t3 - t2:6f}") # :6f for six digit after the decimal point so result will be like 4894778.447759
+
+
